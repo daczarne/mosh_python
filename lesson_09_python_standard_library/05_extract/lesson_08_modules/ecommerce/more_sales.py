9c126c3c7f6c2378427be1cfb78f1d341fa5443c
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2020-12-06 18:06:45 -0300

    working with zip files

---
+++ lesson_09_python_standard_library/05_extract/lesson_08_modules/ecommerce/more_sales.py
@@ -0,0 +1,14 @@
+print("More sales initialized", __name__)
+
+
+def calc_tax():
+    print("Calculate the tax value.")
+
+
+def calc_shipping():
+    print("Calculate the shipping cost.")
+
+
+if __name__ == "__main__":
+    print("More sales started")
+    calc_tax()
---
+++ lesson_09_python_standard_library/05_extract/lesson_08_modules/ecommerce/shopping/even_more_sales.py
@@ -0,0 +1,18 @@
+# Absolut import statement
+from ecommerce.customer import contact
+
+# Relative import statement
+from ..customer import contact
+
+contact.contact_customer()
+
+
+print("Sales initialized")
+
+
+def calc_tax():
+    print("Calculate the tax value.")
+
+
+def calc_shipping():
+    print("Calculate the shipping cost.")
