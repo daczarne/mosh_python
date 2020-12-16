92050cf09a0bfded16416f4ccf9a3b27380f4661
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2020-12-16 02:53:01 -0300

    protects key

--- lesson_12_django/vidly/vidly/settings.py
+++ lesson_12_django/vidly/vidly/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ipjx4!u7dh&25tv@0ohb+8omzecf44#_kmy9)x6w+#1lq903yz'
+SECRET_KEY = secret_key.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
